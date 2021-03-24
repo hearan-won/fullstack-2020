deb91fc802fc4ee8200ace7f6ec0bcbde9c6385d
Hearan Won <won58012@gmail.com>
2021-03-24 17:10:16 -0400

    part 3-a finished

--- part-3/basic-npm/index.js
+++ part-3/basic-npm/index.js
@@ -1,5 +1,6 @@
 // change from http to express (library of node.js)
 const express = require('express')
+const nodemon = require('nodemon')
 const app = express() 
 
 // list of notes in JSON format
@@ -32,7 +33,64 @@ app.get('/', (request, response) => {
 app.get('/api/notes', (request, response) => {
     response.json(notes)
 })
+
+// route with parameter as id 
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note =>
+    //console.log(note.id, typeof note.id, id, typeof id, note.id === id)
+    note.id === id)
+
+  if (note) {
+    response.json(note)
+  } else {
+    // throw not found error
+    response.status(404).end()
+  }
+})
+
+// route for deleting 
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(Request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  // if deleting is successful, respond with status code 204 no content
+  // for simplicity, both cases return 204 error
+  response.status(204).end()
+})
+
+app.use(express.json())
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    // map returns array, the ... spread syntax changes it into individual nums
+    ? Math.max(...notes.map(n => n.id))
+    : 0 
+  return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    
+    // if the important property is missing, default is false
+    important: body.important || false,
+    date : new Date(),
+    id: generateId(),
+  }
   
+  notes = notes.concat(note)
+
+  response.json(note)
+})
 
 const PORT = 3001
 app.listen(PORT)
